7ad19bf9b2b21b0578ef0c13c1b87bfafef55d59
sw4q3ar <biggercsgo@gmail.com>
2022-03-07 12:26:25 +0100

    3.nev deklarálása

--- src/egyenikonfliktus/EgyeniKonfliktus.java
+++ src/egyenikonfliktus/EgyeniKonfliktus.java
@@ -1,8 +1,9 @@
 package egyenikonfliktus;
 public class EgyeniKonfliktus {
     public static void main(String[] args) {
-        String nev1;
-        String nev2;
+        String nev1 = "AP";
+        String nev2 = "Arrnold";
+        String nev3;
     }
     
 }
