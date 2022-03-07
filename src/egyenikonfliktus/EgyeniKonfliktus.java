5ddff1e998350404e53690d1eae60c0fb22def96
sw4q3ar2 <97527467+sw4q3ar2@users.noreply.github.com>
2022-03-07 12:24:51 +0100

    3. név deklarálása

--- src/egyenikonfliktus/EgyeniKonfliktus.java
+++ src/egyenikonfliktus/EgyeniKonfliktus.java
@@ -1,8 +1,10 @@
 package egyenikonfliktus;
 public class EgyeniKonfliktus {
     public static void main(String[] args) {
+        int i = 7;
         String arnold;
         String nev2;
+        int nev3;
     }
     
 }
